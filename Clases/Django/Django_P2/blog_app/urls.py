e1a8d4ca17676e5390c21a0445beba39a6ba12d5
Carlos Moraless <carlos52.7624@gmail.com>
2023-12-18 14:16:02 -0400

    1

--- Clases/Django/Django_P2/blog_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import homepageview, aboutpageview
-
-urlpatterns=[
-    path("",  homepageview.as_view(), name="home"),
-    path("about/", aboutpageview.as_view(), name="about" ) 
-
-]
