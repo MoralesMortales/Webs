a3ff56f53ab105462bb62cf121f763a54b093fe1
Carlos Moraless <carlos52.7624@gmail.com>
2023-12-19 16:48:48 -0400

    New project Django

--- Clases/Django/Django_P3/Testing_dj/Testing_dj_app/views.py
+++ Clases/Django/Django_P3/Testing_dj/Testing_dj_app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homePageViewmifuncion(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Come on pal, we're going well")
