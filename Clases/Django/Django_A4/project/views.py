5af61c717dc1e8bf2149734bebf5494583dad71f
Carlos Moraless <carlos52.7624@gmail.com>
2024-01-05 18:03:14 -0400

    notes

--- Clases/Django/Django_A4/project/views.py
+++ Clases/Django/Django_A4/project/views.py
@@ -2,9 +2,9 @@ from django.views.generic import View
 from django.shortcuts import render
 
 class HomeView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs): #Se coloca por default/ self se aplica para llamarse a si mismo a una def en py
         context = {
             
         }
         
-        return render(request,'index.html', context)
+        return render(request,'index.html', context) #request pide al servidor, lo otro es la dirreccion, y el context el contenido adicional a utilizar
